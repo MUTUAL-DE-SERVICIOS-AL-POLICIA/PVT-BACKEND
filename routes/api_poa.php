a9529d4d42e08a2c549ec32733f0e1e55379dc94
Richard Velasquez <revelasquez666@gmail.com>
2024-02-20 14:55:10 -0400

    cambio de ruta de acceso

--- routes/api_poa.php
+++ routes/api_poa.php
@@ -10,13 +10,12 @@ Route::group([
 ], function () {
     // Rutas abiertas
     Route::post('get_session',[App\Http\Controllers\AllowedMacDevicesController::class, 'get_session']);
+    //Route::post('get_qualification_report',[App\Http\Controllers\ReportController::class, 'download_qualification_report']);
     // Rutas autenticadas con token
     Route::group([
         'middleware' => ['api_poa']
     ], function () {
         Route::get('/get_affiliate_loans/{id_affiliate}',[App\Http\Controllers\Loan\LoanController::class, 'get_information_loan']);
         Route::get('loan/{loan}/print/kardex',[App\Http\Controllers\Loan\LoanController::class, 'print_kardex']);
-        //Reporte de calificacion de servicios
-        Route::post('get_qualification_report',[App\Http\Controllers\ReportController::class, 'download_qualification_report']);
     });
 });
