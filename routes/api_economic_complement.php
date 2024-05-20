e986bdb349e9e1c39ba58ca9cb365cf9434767e9
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-05-20 09:36:06 -0400

    rutas para complemento económico

---
+++ routes/api_economic_complement.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\AuthenticationException;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'economic_complement'
+], function () {
+    // Rutas autenticadas con token
+    Route::group([
+        'middleware' => ['api_auth']
+    ], function () {
+
+    });
+});
