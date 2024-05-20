6d0b8e1461f3b718736ba35841a3f374ab6385fc
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-05-20 10:04:10 -0400

    API para listar las movimientos de pagos en demasía

--- app/Http/Controllers/EconomicComplement/EcoComMovementController.php
+++ app/Http/Controllers/EconomicComplement/EcoComMovementController.php
@@ -14,5 +14,32 @@ class EcoComMovementController extends Controller
 {
     public function index(Request $request,$affiliate_id)
     {
+        $request['affiliate_id']=$affiliate_id;
+        $request->validate([
+            'affiliate_id' => 'required|int',
+        ]);
+        $movements= EcoComMovement::where("affiliate_id",$affiliate_id)->get();
+        $movement_objects= collect();
+        $correlative=1;
+        foreach ($movements as $movement)
+        {
+            $movement_object = new \stdClass();
+            $movement_object->correlative=$correlative++;
+            $movement_object->id=$movement->id;
+            $movement_object->affilate_id=$movement->affiliate_id;
+            $movement_object->description=$movement->description;
+            $movement_object->amount=$movement->amount;
+            $movement_object->balance=$movement->balance;
+            $movement_object->created_at = Carbon::parse($movement->created_at)->format('d-m-Y');
+            $movement_objects->push($movement_object);
+        }
+        return response()->json([
+            'error' => "false",
+            'message' => 'Listado de movimientos de dinero de pagos en demasia',
+            'payload' => [
+                'movements' => $movement_objects
+            ]
+            ]);
     }
+
 }
--- routes/api_economic_complement.php
+++ routes/api_economic_complement.php
@@ -13,5 +13,6 @@ Route::group([
         'middleware' => ['api_auth']
     ], function () {
         Route::get('/eco_com_procedure_list',[App\Http\Controllers\EconomicComplement\EcoComProcedureController::class,'index']);
+        Route::get('/movement_list/{affiliate_id}', [App\Http\Controllers\EconomicComplement\EcoComMovementController::class, 'index']);
     });
 });
