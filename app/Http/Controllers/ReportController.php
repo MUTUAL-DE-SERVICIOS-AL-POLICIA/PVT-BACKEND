a9e3277de0a2a29bd590edb86a8da067c10a7bf4
bdquispe <daysi.quispe444@gmail.com>
2025-06-02 19:01:05 -0400

    Reporte de todos los afiliados y cónyuges

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -46,31 +46,10 @@ class ReportController extends Controller
      */
     public function report_affiliates_spouses(Request $request)
     {
-        $date = date('Y-m-d');
+        ini_set('memory_limit', '2048M');
         $list = DB::table('affiliates')
             ->select(
                 'affiliates.id as nup',
-                'affiliates.identity_card as identity_card',
-                'affiliates.first_name as first_name',
-                'affiliates.second_name as second_name',
-                'affiliates.last_name as last_name',
-                'affiliates.mothers_last_name as mothers_last_name',
-                'affiliates.surname_husband as surname_husband',
-                'affiliates.date_entry as date_entry',
-                'affiliates.birth_date as birth_date',
-                'spouses.identity_card as spouse_identity_card',
-                'spouses.first_name as spouse_first_name',
-                'spouses.second_name as spouse_second_name',
-                'spouses.last_name as spouse_last_name',
-                'spouses.mothers_last_name as spouse_mothers_last_name',
-                'spouses.surname_husband as spouse_surname_husband',
-                DB::raw('DATE(MAX(spouses.created_at)) as spouse_registration_date'),
-                'spouses.birth_date as spouse_birth_date',
-                'affiliates.registration as registration'
-            )
-            ->join('spouses', 'spouses.affiliate_id', '=', 'affiliates.id')
-            ->groupBy(
-                'affiliates.id',
                 'affiliates.identity_card',
                 'affiliates.first_name',
                 'affiliates.second_name',
@@ -78,20 +57,22 @@ class ReportController extends Controller
                 'affiliates.mothers_last_name',
                 'affiliates.surname_husband',
                 'affiliates.date_entry',
-                'affiliates.birth_date', 
-                'spouses.identity_card', 
-                'spouses.first_name',
-                'spouses.second_name', 
-                'spouses.last_name', 
-                'spouses.mothers_last_name',
-                'spouses.surname_husband', 
-                'spouses.birth_date', 
-                'affiliates.registration'
+                'affiliates.birth_date',
+                'spouses.identity_card as spouse_identity_card',
+                'spouses.first_name as spouse_first_name',
+                'spouses.second_name as spouse_second_name',
+                'spouses.last_name as spouse_last_name',
+                'spouses.mothers_last_name as spouse_mothers_last_name',
+                'spouses.surname_husband as spouse_surname_husband',
+                'spouses.created_at as spouse_create_date',
+                'spouses.birth_date as spouse_birth_date',
+                'affiliates.registration as registration',
+                'spouses.registration as registration_spouse'
             )
-            ->orderBy('affiliates.id', 'asc')
-            ->get();
+            ->leftJoin('spouses', 'spouses.affiliate_id', '=', 'affiliates.id')
+            ->orderBy('affiliates.id', 'asc');
 
-        return Excel::download(new AffiliatesSpousesExport($list), 'affiliates_spouses_report.xls');
+        return Excel::download(new AffiliatesSpousesExport($list), 'affiliates_spouses_report.xlsx');
     }
 
     /**
