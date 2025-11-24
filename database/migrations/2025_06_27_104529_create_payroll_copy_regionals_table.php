8907a2a2e4266d9b5a5fe7e7d333217dbdc461af
bdquispe <daysi.quispe444@gmail.com>
2025-11-24 16:07:16 -0400

    modificación de la tabla payroll_copy_regionals en base de datos auxiliar

--- database/migrations/2025_06_27_104529_create_payroll_copy_regionals_table.php
+++ database/migrations/2025_06_27_104529_create_payroll_copy_regionals_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     */
+    */
     public function up(): void
     {
         Schema::connection('db_aux')->create('payroll_copy_regionals', function (Blueprint $table) {
@@ -30,7 +30,6 @@ return new class extends Migration
             $table->decimal('cotizable')->default(0)->comment('Cotizable');
             $table->decimal('aporte')->default(0)->comment('Monto de aporte');
             $table->decimal('porcentaje_aporte')->default(0)->comment('Porcentaje de aporte');
-            $table->unsignedBigInteger('affiliate_id_frcam')->nullable()->comment('Id del afiliado titular según FRCAM');
             $table->string('error_message')->nullable()->comment('Mensaje del error');
             $table->unsignedBigInteger('affiliate_id')->nullable()->comment('Id del afiliado titular');
             $table->enum('state', ['accomplished','unrealized','validated'])->default('unrealized')->comment('Estado si fue encontrado o no encontrado');
@@ -42,7 +41,7 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     */
+    */
     public function down(): void
     {
         Schema::connection('db_aux')->dropIfExists('payroll_copy_regionals');
